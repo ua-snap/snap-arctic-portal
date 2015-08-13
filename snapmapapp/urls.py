918cf0baa40988246f7586d09153d84ffa56d721
Craig Stephenson <cstephen@iarc.uaf.edu>
2015-08-13 12:41:55 -0700

    Override front page URL pattern with custom template.

--- snapmapapp/urls.py
+++ snapmapapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-                       url(r'^snapmapapp$', TemplateView.as_view(template_name='snapmapapp/index.html'), name='snapmapapp'),)
+                       url(r'^/?$', TemplateView.as_view(template_name='snapmapapp/index.html'), name='snapmapapp'),)
 
